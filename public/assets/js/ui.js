d4d21e8a2b06bdc4228b4daa2bd911e07aa1df9a
Aelly Liu <laellyl@gmail.com>
2017-07-29 12:09:02 -0500

    add the empty message for burger eat side

--- public/assets/js/ui.js
+++ public/assets/js/ui.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
     var count = $("#displayAteBurger ul").children().length;
     if(count <= 0)
         {
-            $("#displayAteBurger .panel-body").text("You didn't eat any burger yet!");
+            $("#displayAteBurger .panel-body").text("You have not eaten any burger yet!");
+        }
+
+    if ($("#displayBurger ul").children().length <=0)
+        {
+            $("#displayBurger .panel-body").text("You don't have any burger to eat");
         }
 
 
