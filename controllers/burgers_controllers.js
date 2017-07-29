e65cfe28c6e5384dee89cd529eff994d8c017d93
Aelly Liu <laellyl@gmail.com>
2017-07-29 12:34:07 -0500

    add if to make sure the burger name is not empty

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,55 +1,57 @@
-var express=require("express");
-var burger=require("../models/burger.js");
+var express = require("express");
+var burger = require("../models/burger.js");
 
-var route=express.Router();
+var route = express.Router();
 
 
-route.get("/",function(req,res){
-    burger.selectAll(function(data){
+route.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         console.log(data);
-        res.render("index",{burgers:data});
+        res.render("index", { burgers: data });
     })
-    
+
 });
 
-route.post("/",function(req,res){
-    var data={
-        burger_name:req.body.burgerName,
-        devoured:false
+route.post("/", function (req, res) {
+    if (req.body.burgerName) {
+        var data = {
+            burger_name: req.body.burgerName,
+            devoured: false
+        }
+        burger.insertOne(data, function (result) {
+            if (result) {
+                res.redirect("/");
+            }
+            else {
+                console.log("Insert failed");
+            }
+
+        });
+
     }
-    burger.insertOne(data,function(result){
-        if(result)
-        {
-            res.redirect("/");
-        }
-        else{
-            console.log("Insert failed");
-        }
-        
-    });
+
 });
 
-route.put("/:id",function(req,res){
+route.put("/:id", function (req, res) {
     // if (req.body.devour === "true") {
     //     var data = {
     //         devoured: true
     //     }
     // }
-    var data={
-        devoured:true
+    var data = {
+        devoured: true
     }
 
-    burger.updateOne(data,"id",req.params.id,function(result){
+    burger.updateOne(data, "id", req.params.id, function (result) {
 
-        if(result)
-        {
-            console.log("devoured: "+ result);
+        if (result) {
+            console.log("devoured: " + result);
             res.redirect("/");
         }
-        else{
+        else {
             console.log("update failed");
         }
     });
 });
 
-module.exports=route;
+module.exports = route;
